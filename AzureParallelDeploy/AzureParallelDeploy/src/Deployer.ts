0e540cee3b926ac261da95a696924a1ed1755a49
kumecky <kumecky@kros.sk>
2021-06-10 15:11:26 +0200

    pr changes

--- AzureParallelDeploy/AzureParallelDeploy/src/Deployer.ts
+++ AzureParallelDeploy/AzureParallelDeploy/src/Deployer.ts
@@ -1,9 +1,8 @@
 import tl = require("azure-pipelines-task-lib/task");
-import { AppType, Settings } from './Interfaces'
+import { AppType, Settings } from "./Interfaces";
 import { Utility } from "./Utility";
 
 export class Deployer {
-
 	public constructor(settings: Settings, debug: boolean) {
 		this.settings = settings;
 		this.debug = debug;
@@ -20,30 +19,45 @@ export class Deployer {
 		var deployments: Q.Promise<void>[] = [];
 
 		console.log(tl.loc("DeployingServices"));
-		console.log(tl.loc("DeployingServicesBaseFolder", this.settings.appSourceBasePath));
+		console.log(
+			tl.loc("DeployingServicesBaseFolder", this.settings.appSourceBasePath)
+		);
 		console.log("");
 
-		services.forEach(service => {
+		services.forEach((service) => {
 			service = service.trim();
 			if (service == "") {
 				return;
 			}
-			var targetService = Utility.formatString(this.settings.appNameFormat, service);
-			var sourceFileName = Utility.formatString(this.settings.appSourceFormat, service);
+			var targetService = Utility.formatString(
+				this.settings.appNameFormat,
+				service
+			);
+			var sourceFileName = Utility.formatString(
+				this.settings.appSourceFormat,
+				service
+			);
 
 			console.log("");
 			console.log(tl.loc("DeployingServiceStart", service));
 			console.log(tl.loc("DeployingServiceFilename", sourceFileName));
 			console.log(tl.loc("DeployingServiceAzureName", targetService));
 
-			var sourceFiles = tl.findMatch(this.settings.appSourceBasePath, `**/${sourceFileName}`);
+			var sourceFiles = tl.findMatch(
+				this.settings.appSourceBasePath,
+				`**/${sourceFileName}`
+			);
 			if (sourceFiles.length == 0) {
 				result = false;
-				Utility.logError(tl.loc("DeployingServiceNoSourceFile", sourceFileName, service));
+				Utility.logError(
+					tl.loc("DeployingServiceNoSourceFile", sourceFileName, service)
+				);
 				return;
 			} else if (sourceFiles.length > 1) {
 				result = false;
-				Utility.logError(tl.loc("DeployingServiceMoreSourceFiles", sourceFileName, service));
+				Utility.logError(
+					tl.loc("DeployingServiceMoreSourceFiles", sourceFileName, service)
+				);
 				for (const file of sourceFiles) {
 					console.error(`  - ${file}`);
 				}
@@ -51,11 +65,13 @@ export class Deployer {
 			}
 			console.log(tl.loc("DeployingServiceUsingSourceFile", sourceFiles[0]));
 
-			var command = this.settings.appType == AppType.FunctionApp ? "functionapp" : "webapp";
+			var command =
+				this.settings.appType == AppType.FunctionApp ? "functionapp" : "webapp";
 			var slotNameParam = Utility.isNullOrWhitespace(this.settings.slotName)
 				? ""
 				: ` --slot ${this.settings.slotName}`;
-			var azArgs = `${command} deployment source config-zip` +
+			var azArgs =
+				`${command} deployment source config-zip` +
 				` --resource-group ${this.settings.resourceGroup}` +
 				` --name ${targetService}${slotNameParam}` +
 				` --src "${sourceFiles[0]}"`;
@@ -69,28 +85,31 @@ export class Deployer {
 		return result;
 	}
 
-	public ExecuteDeployment(azArgs: string, service: string, result: boolean): any
-	{
-		return tl.exec("az", azArgs)
-		.then(
-			result => {
-				console.log(`${service}: ${tl.loc("DeployingServiceOk")}`)
+	private ExecuteDeployment(
+		azArgs: string,
+		service: string,
+		result: boolean
+	): any {
+		return tl.exec("az", azArgs).then(
+			(_) => {
+				console.log(`${service}: ${tl.loc("DeployingServiceOk")}`);
 			},
-			error => {
+			(error) => {
 				let retryCount = this.retryCount[service];
-				if (this.retryCount[service] < Deployer.maxRetries)
-				{
+				if (this.retryCount[service] < Deployer.maxRetries) {
 					this.retryCount[service]++;
-					console.log(`${service}: ${tl.loc("DeployServiceRetry", retryCount)}`)
+					console.log(
+						`${service}: ${tl.loc("DeployServiceRetry", retryCount)}`
+					);
 					this.ExecuteDeployment(azArgs, service, result);
 				} else {
 					result = false;
-					if (this.debug) {
-						Utility.logError(error);
-					}
 					tl.error(`${service}: ${tl.loc("DeployingServiceError")}`);
 				}
-				
+
+				if (this.debug) {
+					Utility.logError(error);
+				}
 			}
 		);
 	}
