335d83a132846bd919b3fc424f89a290f875934e
Stano Peťko <stano@stanopetko.eu>
2021-06-11 16:16:25 +0200

    Some fixes

--- AzureParallelDeploy/AzureParallelDeploy/src/Deployer.ts
+++ AzureParallelDeploy/AzureParallelDeploy/src/Deployer.ts
@@ -19,45 +19,30 @@ export class Deployer {
 		var deployments: Q.Promise<void>[] = [];
 
 		console.log(tl.loc("DeployingServices"));
-		console.log(
-			tl.loc("DeployingServicesBaseFolder", this.settings.appSourceBasePath)
-		);
+		console.log(tl.loc("DeployingServicesBaseFolder", this.settings.appSourceBasePath));
 		console.log("");
 
-		services.forEach((service) => {
+		services.forEach(service => {
 			service = service.trim();
 			if (service == "") {
 				return;
 			}
-			var targetService = Utility.formatString(
-				this.settings.appNameFormat,
-				service
-			);
-			var sourceFileName = Utility.formatString(
-				this.settings.appSourceFormat,
-				service
-			);
+			var targetService = Utility.formatString(this.settings.appNameFormat, service);
+			var sourceFileName = Utility.formatString(this.settings.appSourceFormat, service);
 
 			console.log("");
 			console.log(tl.loc("DeployingServiceStart", service));
 			console.log(tl.loc("DeployingServiceFilename", sourceFileName));
 			console.log(tl.loc("DeployingServiceAzureName", targetService));
 
-			var sourceFiles = tl.findMatch(
-				this.settings.appSourceBasePath,
-				`**/${sourceFileName}`
-			);
+			var sourceFiles = tl.findMatch(this.settings.appSourceBasePath, `**/${sourceFileName}`);
 			if (sourceFiles.length == 0) {
 				result = false;
-				Utility.logError(
-					tl.loc("DeployingServiceNoSourceFile", sourceFileName, service)
-				);
+				Utility.logError(tl.loc("DeployingServiceNoSourceFile", sourceFileName, service));
 				return;
 			} else if (sourceFiles.length > 1) {
 				result = false;
-				Utility.logError(
-					tl.loc("DeployingServiceMoreSourceFiles", sourceFileName, service)
-				);
+				Utility.logError(tl.loc("DeployingServiceMoreSourceFiles", sourceFileName, service));
 				for (const file of sourceFiles) {
 					console.error(`  - ${file}`);
 				}
@@ -65,13 +50,11 @@ export class Deployer {
 			}
 			console.log(tl.loc("DeployingServiceUsingSourceFile", sourceFiles[0]));
 
-			var command =
-				this.settings.appType == AppType.FunctionApp ? "functionapp" : "webapp";
+			var command = this.settings.appType == AppType.FunctionApp ? "functionapp" : "webapp";
 			var slotNameParam = Utility.isNullOrWhitespace(this.settings.slotName)
 				? ""
 				: ` --slot ${this.settings.slotName}`;
-			var azArgs =
-				`${command} deployment source config-zip` +
+			var azArgs = `${command} deployment source config-zip` +
 				` --resource-group ${this.settings.resourceGroup}` +
 				` --name ${targetService}${slotNameParam}` +
 				` --src "${sourceFiles[0]}"`;
@@ -85,22 +68,16 @@ export class Deployer {
 		return result;
 	}
 
-	private ExecuteDeployment(
-		azArgs: string,
-		service: string,
-		result: boolean
-	): any {
+	private ExecuteDeployment(azArgs: string, service: string, result: boolean): any {
 		return tl.exec("az", azArgs).then(
-			(_) => {
+			_ => {
 				console.log(`${service}: ${tl.loc("DeployingServiceOk")}`);
 			},
-			(error) => {
+			error => {
+				this.retryCount[service]++;
 				let retryCount = this.retryCount[service];
-				if (this.retryCount[service] < Deployer.maxRetries) {
-					this.retryCount[service]++;
-					console.log(
-						`${service}: ${tl.loc("DeployServiceRetry", retryCount)}`
-					);
+				if (retryCount <= Deployer.maxRetries) {
+					console.log(`${service}: ${tl.loc("DeployServiceRetry", retryCount)}`);
 					this.ExecuteDeployment(azArgs, service, result);
 				} else {
 					result = false;
